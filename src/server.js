dd55b656c20bab8f03ad83c822ed15953c4b3090
Tomas Braz <tomasbraz43@hotmail.com>
2022-12-12 00:20:10 -0300

    Falta estilizar os modais

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const routes = require('./router');
+const path = require('path');
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+app.use(express.static(path.join(process.cwd(), 'public')));
 
 app.set("view engine", 'ejs');
 app.set("views",  "./views");
