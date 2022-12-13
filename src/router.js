dcc4e31cff89efa1e820be3e14a4fbb8667495da
Tomas Braz da Silva <20221ewbj0157@belojardim.ifpe>
2022-12-13 15:30:15 -0300

    Adicionando elementos css

--- src/router.js
+++ src/router.js
@@ -3,5 +3,11 @@ const router = require("express").Router();
 const taskController = require('./controllers/taskController');
 
 router.get('/', taskController.createTask);
+// router.post('/createtask', taskController.createTask);
+// router.get('/alltask', taskController.index);
+// router.get('/gettask/:taskId', taskController.getTask);
+// router.post('/updatetask/:taskId', taskController.update);
+// router.get('/deletetask/:taskId', taskController.destroy);
+// router.post('/finishtask/:taskId', taskController.finishTask);
 
 module.exports = router;
